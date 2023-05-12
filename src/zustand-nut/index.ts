92c9a468cc0f9998f19bb7381dbea3d7c756cdea
bubucuo <276185876@qq.com>
2023-05-12 16:06:26 +0800

    save

--- src/zustand-nut/index.ts
+++ src/zustand-nut/index.ts
@@ -1,9 +1,5 @@
-// import {useSyncExternalStore} from "react";
-import type {StateCreator, StoreApi} from "./vanilla";
-import {createStore} from "./vanilla";
-import useSyncExternalStoreExports from "use-sync-external-store/shim/with-selector";
-
-const {useSyncExternalStoreWithSelector} = useSyncExternalStoreExports;
+import {useSyncExternalStore} from "react";
+import {StateCreator, StoreApi, createStore} from "./vanilla";
 
 type ExtractState<S> = S extends {getState: () => infer T} ? T : never;
 
@@ -13,30 +9,20 @@ type WithReact<S extends ReadonlyStoreApi<unknown>> = S & {
   getServerState?: () => ExtractState<S>;
 };
 
-export function create<T>(createState: StateCreator<T> | undefined) {
-  return createState ? createImpl(createState) : createImpl;
+export function create<T>(createState: StateCreator<T>) {
+  return createImpl(createState);
 }
 
 function createImpl<T>(createState: StateCreator<T>) {
   const api = createStore(createState);
 
-  const useBoundStore: any = (selector?: any) => useStore(api, selector);
+  const useBoundStore: any = () => useStore(api);
 
   return useBoundStore;
 }
 
-export function useStore<TState, StateSlice>(
-  api: WithReact<StoreApi<TState>>,
-  selector: (state: TState) => StateSlice = api.getState as any
-) {
-  // const slice = useSyncExternalStore(api.subscribe, api.getState);
-
-  const slice = useSyncExternalStoreWithSelector(
-    api.subscribe,
-    api.getState,
-    api.getServerState || api.getState,
-    selector
-  );
+export function useStore<TState>(api: WithReact<StoreApi<TState>>) {
+  const slice = useSyncExternalStore(api.subscribe, api.getState);
 
   return slice;
 }
--- src/zustand-nut/vanilla.ts
+++ src/zustand-nut/vanilla.ts
@@ -3,7 +3,6 @@ export interface StoreApi<T> {
     partial: T | Partial<T> | {_(state: T): T | Partial<T>}["_"],
     replace?: boolean | undefined
   ) => void;
-
   getState: () => T;
   subscribe: (listener: (state: T, prevState: T) => void) => () => void;
   destroy: () => void;
@@ -14,44 +13,44 @@ export type StateCreator<T> = (
   getState: StoreApi<T>["getState"]
 ) => T;
 
-export const createStore = (createState: any) => createStoreImpl(createState);
-
-const createStoreImpl = (createState: any) => {
+export const createStore = (createState: any) => {
   type TState = ReturnType<typeof createState>;
-  let state: TState;
   type Listener = (state: TState, prevState: TState) => void;
 
+  let state: TState;
   const listeners: Set<Listener> = new Set();
 
   const setState: StoreApi<TState>["setState"] = (partial, replace) => {
-    const nextState =
-      typeof partial === "function"
-        ? (partial as (state: TState) => TState)(state)
-        : partial;
+    const nextState = typeof partial === "function" ? partial(state) : partial;
+
     if (!Object.is(nextState, state)) {
       const previousState = state;
-      // 如果replace为true，或者nextState不是对象，则不是合并，而是直接替换旧state
       state =
         replace ?? typeof nextState !== "object"
-          ? (nextState as TState)
+          ? nextState
           : Object.assign({}, state, nextState);
+
       listeners.forEach((listener) => listener(state, previousState));
     }
   };
-
   const getState: StoreApi<TState>["getState"] = () => state;
-
-  const subscribe: StoreApi<TState>["subscribe"] = (listener) => {
+  const subscribe: StoreApi<TState>["subscribe"] = (listener: Listener) => {
     listeners.add(listener);
-    // Unsubscribe
+
     return () => listeners.delete(listener);
   };
-
   const destroy: StoreApi<TState>["destroy"] = () => {
     listeners.clear();
   };
 
-  const api = {setState, getState, subscribe, destroy};
-  state = createState(setState, getState, api);
-  return api as any;
+  const api = {
+    getState,
+    setState,
+    destroy,
+    subscribe,
+  };
+
+  state = createState(setState, getState);
+
+  return api;
 };
