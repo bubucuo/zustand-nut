cf2d3faa2235663595d7df8b6ebbb55e73b1f0bf
bubucuo <276185876@qq.com>
2023-05-15 04:00:36 +0800

    save

--- src/zustand-nut/react.ts
+++ src/zustand-nut/react.ts
@@ -12,17 +12,6 @@ import useSyncExternalStoreExports from "use-sync-external-store/shim/with-selec
 
 const {useSyncExternalStoreWithSelector} = useSyncExternalStoreExports;
 
-// export function create<T>(createState?: StateCreator<T, [], []> | undefined) {
-//   return createState ? createImpl(createState) : createImpl;
-// }
-
-// type Create = {
-//   <T>(initializer: StateCreator<T, [], []>): any;
-//   // <T>(
-//   //   initializer: StateCreator<T>
-//   // ) => any; // UseBoundStore<Mutate<StoreApi<T>, Mos>>
-// };
-
 type ExtractState<S> = S extends {getState: () => infer T} ? T : never;
 
 type ReadonlyStoreApi<T> = Pick<StoreApi<T>, "getState" | "subscribe">;
@@ -41,10 +30,10 @@ export type UseBoundStore<S extends WithReact<ReadonlyStoreApi<unknown>>> = {
 
 type Create = {
   <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
-    initializer: StateCreator<T, [], Mos>
+    initializer?: StateCreator<T, [], Mos>
   ): UseBoundStore<Mutate<StoreApi<T>, Mos>>;
   <T>(): <Mos extends [StoreMutatorIdentifier, unknown][] = []>(
-    initializer: StateCreator<T, [], Mos>
+    initializer?: StateCreator<T, [], Mos>
   ) => UseBoundStore<Mutate<StoreApi<T>, Mos>>;
   /**
    * @deprecated Use `useStore` hook to bind store
@@ -52,17 +41,20 @@ type Create = {
   <S extends StoreApi<unknown>>(store: S): UseBoundStore<S>;
 };
 
-export const create: Create = function <T>(
-  createState: StateCreator<T, [], []> | undefined
+export const create: any = function <T>(
+  createState?: any //StateCreator<T, [], []> | undefined
 ) {
   return createState ? createImpl(createState) : createImpl;
 };
 
-export function createImpl<T>(createState: StateCreator<T, [], []>) {
+function createImpl<T>(createState: StateCreator<T, [], []>) {
   const api =
     typeof createState === "function" ? createStore(createState) : createState;
 
-  const useBoundStore: any = (selector: any) => useStore(api, selector);
+  const useBoundStore: any = (selector?: any, equalityFn?: any) =>
+    useStore(api, selector, equalityFn);
+
+  Object.assign(useBoundStore, api);
 
   return useBoundStore;
 }
